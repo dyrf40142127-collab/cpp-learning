b6b0c39d611e1f89521319ac298f08057ba8202c
yuzixuan <dyrf40142127@gmail.com>
2026-06-11 21:44:57 +0800

    Add class example

---
+++ CPP/Basic/class/class example/class example.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+class Player
+{
+public:
+	int x, y;
+	int speed;
+	void Move(int xa, int ya) {
+		x += xa * speed;
+		y += ya * speed;
+	}
+};
+int main()
+{
+	Player player;//前面为类，后面为实例后的对象
+	player.x = 5;//通过.来对类中的变量进行访问
+	player.y = 4;
+	player.speed = 3;
+	player.Move(1, -1);
+	std::cout << player.x <<" "<< player.y << std::endl;
+	return 0;
+
+
+
+}
